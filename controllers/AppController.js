b25648ba9871e68905879f5cbe11cf56f16e01a8
Chris <chris85gillis@gmail.com>
2024-07-10 14:02:42 -0500

    got task 2 working

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,14 +1,14 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-class AppController {
-  static getStatus(req, res) {
+const AppController = {
+  async getStatus(req, res) {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
     res.status(200).json({ redis: redisAlive, db: dbAlive });
-  }
+  },
 
-  static async getStats(req, res) {
+  async getStats(req, res) {
     try {
       const usersCount = await dbClient.nbUsers();
       const filesCount = await dbClient.nbFiles();
@@ -17,7 +17,7 @@ class AppController {
       console.error('Error fetching stats:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
-  }
-}
+  },
+};
 
-export default AppController;
+module.exports = AppController;
